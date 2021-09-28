2fc5f9d53b9c1458dec7e06073a8a717f648d638
Gustavo Henrique Pio <ghenriquepio@gmail.com>
2021-09-27 22:59:35 -0300

    Initializing the repo

---
+++ src/main/java/br/com/gustavo/currencyquoteapishx/CurrencyQuoteApiShxApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.currencyquoteapishx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyQuoteApiShxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyQuoteApiShxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/currencyquoteapishx/CurrencyQuoteApiShxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.currencyquoteapishx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyQuoteApiShxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
